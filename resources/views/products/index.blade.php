817f7d1c96349065692d79871bff6ca44d1c0669
Alaba mustapha <alabamustapha@gmail.com>
2017-08-16 08:28:34 +0000

    product page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -33,7 +33,7 @@
                     <div class="col-md-3 col-sm-4">
                         <div class="product">
                             <div class="image">
-                                <a href="shop-detail.html">
+                                <a href="#">
                                     <img src="img/product1.jpg" alt="" class="img-responsive image1">
                                 </a>
                             </div>
